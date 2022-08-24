e3438b67415d3638891a42233de459344974c9b4
Avani Joshi <avaniklsjoshi@gmail.com>
2022-08-24 11:48:16 +0200

    frontend app removed

--- mflix-ui/src/actions/miscActions.js
+++
@@ -1,9 +0,0 @@
-import * as types from "./actionTypes"
-
-export function toggleDrawer() {
-  return { type: types.TOGGLE_DRAWER }
-}
-
-export function clearError(key) {
-  return { type: types.CLEAR_ERROR, key }
-}
--- mflix-ui/src/routing/ConnectedSwitch.js
+++
@@ -1,7 +0,0 @@
-import { connect } from "react-redux"
-import { Switch } from "react-router-dom"
-const ConnectedSwitch = connect(state => ({
-  location: state.location,
-}))(Switch)
-
-export default ConnectedSwitch
